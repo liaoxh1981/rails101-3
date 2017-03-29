31bef6a59ad6d78c0b4cf9a24366a1a003d97f50
廖晓华“git config --global user.email “liaoxh@gmail.com <liaoxh1981@gmail.com>
2017-03-29 20:29:57 +0800

    add post and hook group/user relation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_andcheck_permission, only: [:edit,update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
       @groups = Group.all
     end
@@ -41,7 +41,7 @@ class GroupsController < ApplicationController
     end
     private
 
-    def find_group_andcheck_permission
+    def find_group_and_check_permission
       @group = Group.find(params[:id])
 
       if current_user != @group.user
